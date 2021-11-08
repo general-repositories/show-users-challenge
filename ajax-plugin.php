a904602c192985b32c8f168bbc83ba62a5dfa637
Austin Reilly <areilly@alphaworks.tech>
2021-11-08 11:44:13 -0800

    removed some spaces

--- ajax-plugin.php
+++ ajax-plugin.php
@@ -8,9 +8,9 @@
 	Author URI: https://ww.a-reilly.dev
 */
 
-function my_user_vote() {
+function my_user_vote(){
 	
-	if (!wp_verify_nonce($_REQUEST['nonce'], "my_user_vote_nonce")) {
+	if(!wp_verify_nonce($_REQUEST['nonce'], "my_user_vote_nonce")){
 		exit("No naughty business please");
 	}
 	
@@ -20,22 +20,22 @@ function my_user_vote() {
 
 	$vote = update_post_meta($_REQUEST["post_id"], "votes", $new_vote_count);
 
-	if($vote === false) {
+	if($vote === false){
 		$result['type'] = "error";
 		$result['vote_count'] = $vote_count;
 	}
-	else {
+	else{
 		$result['type'] = "success";
 		$result['vote_count'] = $new_vote_count;
 	}
 	
-	if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+	if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
 		$result['server'] = $_SERVER;
 		$result['request'] = $_REQUEST;
 		$result = json_encode($result);
 		echo $result;
 	}
-	else {
+	else{
 		header("Location: ".$_SERVER["HTTP_REFERER"]);
 	}
 	
@@ -45,7 +45,7 @@ function my_user_vote() {
 // This guy is hooking my_user_vote into the admin-ajax file
 add_action("wp_ajax_my_user_vote", "my_user_vote");
 
-function my_must_login() {
+function my_must_login(){
 	$result['type'] = "error";
 	$result = json_encode($result);
 	echo $result;
@@ -55,7 +55,7 @@ function my_must_login() {
 add_action("wp_ajax_nopriv_my_user_vote", "my_must_login");
 
 
-function my_script_enqueuer() {
+function my_script_enqueuer(){
 	wp_register_script("my_voter_script", WP_PLUGIN_URL.'/javascript-wordpress-ajax-call/my_voter_script.js');
 	
 	// This seems like the key to the communication between ajax and jquery
