5e16ab9e844cfc3e3579cfc1bd2f685035d1bd6a
Austin Reilly <areilly@alphaworks.tech>
2021-11-23 15:09:14 -0800

    got rid of the extra function idk what the heck the encoded uri was for but it's not actually necssary, just encode it in the body

--- show_authors_aj.js
+++ show_authors_aj.js
@@ -6,10 +6,15 @@ function showUsers(){
 
 	if(!isRan){
 		isRan = true;
-		fetchAjax(myAjax.ajaxurl, {
-			'action': 'show_authors',
-			'nonce': div.getAttribute('data-nonce')
+		fetch(myAjax.ajaxurl, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+			},
+			body: `action=show_authors&nonce=${div.getAttribute('data-nonce')}`
 		})
+		.then(res=>res.json())
 		.then(object=>{
 			if(object.type != 'must login'){
 				for (const key in object){
@@ -21,35 +26,4 @@ function showUsers(){
 			else alert('you must be logged in to see the list');
 		});
 	}
-}
-
-// Dynamic AJAX Fetch Function lolies
-async function fetchAjax(url, body){
-	let bodyString = ``;
-	let overOne = 1;
-	
-	for(const key in body){
-		overOne === 1 ? 
-			bodyString = bodyString + `${key}=${body[key]}` :
-			bodyString = bodyString + `&${key}=${body[key]}`;
-
-		overOne++;
-	}
-	
-	const encodedBody = encodeURI(bodyString);
-
-	const response = fetch(url, {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-		},
-		body: encodedBody
-	});
-
-	const data = (await response).json();
-
-	const object = await data;
-
-	return object;
 }
