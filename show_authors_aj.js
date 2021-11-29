c6ea751ffcbdfe2f2425c331e80c1b82e3cedfc6
Austin Reilly <areilly@alphaworks.tech>
2021-11-29 15:38:31 -0800

    refactored for the new object

--- show_authors_aj.js
+++ show_authors_aj.js
@@ -3,26 +3,34 @@ let isRan = false;
 function showUsers(){
 	const userList = document.getElementById('userList');
 	const div = document.getElementById('showUsers');
+	const today = new Date();
+	const dateString = `${today.getHours()}.${today.getMinutes()}.${today.getSeconds()}`;
+	console.log(dateString);
 
-	if(!isRan){
-		isRan = true;
-		fetch(myAjax.ajaxurl, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-			},
-			body: `action=show_authors&nonce=${div.getAttribute('data-nonce')}`
-		})
-		.then(res=>res.json())
-		.then(object=>{
-			if(object.type != 'must login'){
-				for (const key in object){
+	fetch(myAjax.ajaxurl, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+		},
+		body: `action=show_authors&nonce=${div.getAttribute('data-nonce')}&post_id=${div.getAttribute('post-id')}&user=${div.getAttribute('user')}&time=${dateString}`
+	})
+	.then(res=>res.json())
+	.then(array=>{
+		if(array.type != 'must login'){
+			if(!isRan){
+				isRan = true;
+				for (const key in array){
 					const element = document.createElement('li');
-					element.innerText = object[key];
+					element.innerText = `Clicked ${array[key].click_number} times, click at ${array[key].click_time}`;
 					userList.appendChild(element);
 				}
 			}
-			else alert('you must be logged in to see the list');
-		});
-	}
+			else{
+				const element = document.createElement('li');
+				element.innerText = `Clicked ${array[array.length - 1].click_number} times, click at ${array[array.length - 1].click_time}`;
+				userList.appendChild(element);
+			}
+		}
+		else alert('you must be logged in to see the list');
+	});
 }
